64b3a1aefe70332ed298769d8c213cc9e4ab8e8b
PARTH PARMAR <83713781+parthparmar08@users.noreply.github.com>
2021-12-19 22:13:14 +0530

    Delete Assignment3/1 directory

--- Assignment3/1/1/model.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const Student = new mongoose.Schema({
-  name: { type: String, default: null },
-  email: { type: String, default: null },
-  password: { type: String, default: null },
-});
-
-module.exports = mongoose.model("Student", Student);
